4ebdc79b1764c0540f4f33109c26247ed336d1d0
kgyang <kaigeyang@sina.com>
2017-07-06 16:20:52 +0800

    remove useless

--- cli.py
+++ cli.py
@@ -5,12 +5,20 @@ import telnetlib
 
 __all__ = ["Cli"]
 
+CLI_LOGIN='xxxxx'
+CLI_LOGIN_PASSWORD='yyyyy'
 class Cli:
-    explist = [(r'login: ', 'cli'),\
-               (r'Username: ', 'admin'),\
-               (r'Password: ', 'admin'),\
-               (r'Do you acknowledge\? \(Y/N\)\?', 'y'),\
-               (r'# ', '')]
+    oldLoginStyle = [(r'login: ', 'cli'),\
+                     (r'Password: ', 'cli'),\
+                     (r'Username: ', CLI_LOGIN),\
+                     (r'Password: ', CLI_LOGIN_PASSWORD),\
+                     (r'Do you acknowledge\? \(Y/N\)\?', 'y'),\
+                     (r'# ', '', True)]
+    loginStyle = [(r'login: ', 'cli'),\
+                  (r'Username: ', CLI_LOGIN),\
+                  (r'Password: ', CLI_LOGIN_PASSWORD),\
+                  (r'Do you acknowledge\? \(Y/N\)\?', 'y'),\
+                  (r'# ', '')]
 
     def __init__(self, host, port=23, verbose=False):
         self.host = host
@@ -18,7 +26,9 @@ class Cli:
         self.verbose = verbose
         self.tn = None
         self.prompt = r'# '
-        self.login()
+        if not self.login(self.loginStyle):
+            print 'NE runs old software, retry login with old style'
+            self.login(self.oldLoginStyle)
 
     def __del__(self):
         self.logout()
@@ -30,32 +40,36 @@ class Cli:
                 else: self.tn.set_debuglevel(0)
             self.verbose = verb
 
-    def login(self):
+    def login(self, loginStyle):
         if self.tn: return
         self.tn = telnetlib.Telnet(self.host, self.port)
         if not self.tn:
             print 'connect fail'
-            return
+            return False
         if self.verbose: self.tn.set_debuglevel(1)
         complete = False
-        for exp in self.explist:
+        for style in loginStyle: 
             try:
-                result = self.tn.expect([exp[0]], 30)
+                exp = style[0]
+                resp = style[1]
+                result = self.tn.expect([exp], 5)
                 if result[0] == -1:
-                    print 'expect', exp[0], 'timeout'
+                    print 'expect', exp, 'timeout'
                     break
-                if exp[1]: self.tn.write(exp[1] + '\n')
+                else:
+                    if resp: self.tn.write(resp + '\n')
             except EOFError, detail:
                 self.close()
-                print exp[0], exp[1], ":", detail
+                print exp, resp, ":", detail
                 break
             else:
-                complete = (exp == self.explist[-1])
+                complete = (style == loginStyle[-1])
         if not complete:
             self.close()
-            print 'login fail'
+            return False
         else:
             self.getprompt()
+            return True
 
     def logout(self):
         if self.tn:
@@ -82,19 +96,28 @@ class Cli:
         if not self.connected(): return (False, None)
         cmd = ' '.join(cmd.split()) # remove abundant space/tab
         if not exp: exp = [self.prompt]
+        exp.append(r'more\? y=\[yes\] q=\[quit\].*')
         try:
             self.tn.write(cmd + '\n')
-            result = self.tn.expect(exp, 60)
-            if result[0] == -1:
-                self.close()
-            else:
-                # remove the headline(which is cmd), tailline(which is prompt) 
-                # and the empty lines
-                return (True, filter(lambda x: x, result[2].splitlines()[1:-1]))
+            result = list()
+            while True:
+                resp = self.tn.expect(exp, 60)
+                if resp[0] == -1:
+                    self.close()
+                    return (False, None)
+                elif resp[0] == (len(exp) - 1): # paging
+                    # remove the the tailline(which is paging indication) 
+                    result += resp[2].splitlines()[:-1]
+                    self.tn.write('y')
+                else:
+                    result += resp[2].splitlines()
+                    break
         except EOFError, detail:
             print 'running', cmd, "fail :", detail
             self.close()
-        return (False, None)
+            return (False, None)
+        # remove the headline(which is cmd), tailline(which is prompt) and empty lines  
+        return (True, filter(lambda x: x, result[1:-1]))
 
     def runmultiple(self, cmds):
         text = list()
@@ -142,85 +165,9 @@ def test():
         cli = Cli(host, 23, verbose)
         #print cli.runone('show general name')
         print cli.runmultiple('''
-            show general name
             show general date
             show version''')
-        #print cli.runfile('/home/kgyang/oamcli')
-
-def cportstr(shelf, slot, port):
-    return str(shelf) + '/' + str(slot) + '/c' + str(port)
-
-def lportstr(shelf, slot, port):
-    return str(shelf) + '/' + str(slot) + '/l' + str(port)
-
-def test2():
-    shelf = 2
-    slot = 3
-    cportnum = 1 
-    lportnum = 2
-    cmdlist = list()
-    # card
-    cmdlist.append('config slot ' + str(shelf) + '/' + str(slot) + ' type 11dpe12')
-    cmdlist.append('config card ' + str(shelf) + '/' + str(slot) + ' mode qinq')
-    # port type
-    for i in range(1,cportnum + 1):
-        cport = cportstr(shelf, slot, i)
-        cmdlist.append('config interface ' + cport + ' type 1GbE')
-    for i in range(1,lportnum + 1):
-        lport = lportstr(shelf, slot, i)
-        cmdlist.append('config interface ' + lport + ' type otu2')
-    # vts and vts xc and esncp
-    for i in range(1,cportnum + 1):
-        cport = cportstr(shelf, slot, i)
-        cvts = cport + '/1'
-        # cvts
-        cmdlist.append('config interface ' + cport + ' vts1 egress cevlanid all')
-        cmdlist.append('config interface ' + cport + ' vts1 ingress cevlanid all')
-        for j in range(1,lportnum + 1):
-            lport = lportstr(shelf, slot, j)
-            # lvts
-            cmdlist.append('config interface ' + lport + ' vts' + str(i) + ' egress svlanid ' + str(i))
-            cmdlist.append('config interface ' + lport + ' vts' + str(i) + ' ingress svlanid ' + str(i))
-        l1vts = lportstr(shelf, slot, 1) + '/' + str(i)
-        l2vts = lportstr(shelf, slot, 2) + '/' + str(i)
-        # vts xc
-        cmdlist.append('config vtsxc ' + cvts + ' ' + l1vts + ' create ' + \
-                       cvts + '-' + l1vts + ' profile cir 100 pir 100')
-        cmdlist.append('config vtsxc ' + cvts + ' ' + l1vts + ' state up')
-        cmdlist.append('config vtsxc ' + l1vts + ' ' + cvts + ' create ' + \
-                       l1vts + '-' + cvts + ' profile cir 100 pir 100')
-        cmdlist.append('config vtsxc ' + l1vts + ' ' + cvts + ' state up')
-        # esncp
-        cmdlist.append('config aps group ' + str(i) + ' create ' + cvts + ' ' + l2vts + ' ' + l1vts + ' uni')
-    rc, results = Cli('127.0.0.1').runmultiple(cmdlist)
-
-def test3():
-    shelf = 1
-    slot = 8
-    cportnum = 1
-    lportnum = 2
-    cmdlist = list()
-    for i in range(1,cportnum + 1):
-        cport = cportstr(shelf, slot, i)
-        cvts = cport + '/1'
-        l1vts = lportstr(shelf, slot, 1) + '/' + str(i)
-        # esncp
-        cmdlist.append('config aps group ' + str(i) + ' delete yes')
-        # vts xc
-        cmdlist.append('config vtsxc ' + cvts + ' ' + l1vts + ' state down')
-        cmdlist.append('config vtsxc ' + cvts + ' ' + l1vts + ' delete')
-        cmdlist.append('config vtsxc ' + l1vts + ' ' + cvts + ' state down')
-        cmdlist.append('config vtsxc ' + l1vts + ' ' + cvts + ' delete')
-        # cvts
-        cmdlist.append('config interface ' + cport + ' vts1 egress cevlanid none')
-        cmdlist.append('config interface ' + cport + ' vts1 ingress cevlanid none')
-        for j in range(1,lportnum + 1):
-            lport = lportstr(shelf, slot, j)
-            # lvts
-            cmdlist.append('config interface ' + lport + ' vts' + str(i) + ' egress svlanid none')
-            cmdlist.append('config interface ' + lport + ' vts' + str(i) + ' ingress svlanid none')
-    ret, results = Cli('127.0.0.1').runmultiple(cmdlist)
 
 if __name__ == '__main__':
-    test2()
+    test()
 
