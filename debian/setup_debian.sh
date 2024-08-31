e280b2200a1ad8857584fdcac83397a61c7e9b07
Yang Kai <kaigeyang@sina.com>
2024-08-31 10:48:39 +0800

    add mount

--- debian/setup_debian.sh
+++ debian/setup_debian.sh
@@ -1,5 +1,8 @@
+#!/usr/bin/bash
 
-sudo apt install gcc gdb vim neovim make cmake gawk git golang rustc neofetch ffmpeg keepass2 vlc
+[[ -d $HOME/script ]] && exit
+
+sudo apt install gcc gdb vim neovim make cmake gawk git golang rustc neofetch ffmpeg podman qemu-system-arm qemu-system-x86 qemu-utils qemu-user-static keepass2 vlc fcitx5 fcitx5-pinyin
 
 sudo mkdir -p /mnt/win/e /mnt/win/f
 sudo mount -t ntfs-3g /dev/sda5 /mnt/win/e
@@ -14,4 +17,3 @@ cd -
 
 git config --global user.name 'Yang Kai'
 git config --global user.emal 'kaigeyang@sina.com'
-
