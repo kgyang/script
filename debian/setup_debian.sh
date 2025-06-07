f0f7ae30acaaec995efe282037dba899b39cde56
Yang Kai <kaige.yang@163.com>
2025-06-07 11:16:59 +0800

    update debian bashrc

--- debian/setup_debian.sh
+++ debian/setup_debian.sh
@@ -2,7 +2,7 @@
 
 [[ -d $HOME/script ]] && exit
 
-sudo apt install gcc gdb vim neovim make cmake gawk git golang rustc neofetch ffmpeg podman qemu-system-arm qemu-system-x86 qemu-utils qemu-user-static keepass2 vlc fcitx5 fcitx5-pinyin
+sudo apt install gcc gdb vim neovim make cmake gawk git golang rustc neofetch ffmpeg podman qemu-system-arm qemu-system-x86 qemu-utils qemu-user-static keepass2 vlc fcitx5 fcitx5-pinyin python3-pip
 
 sudo mkdir -p /mnt/win/e /mnt/win/f
 sudo mount -t ntfs-3g /dev/sda5 /mnt/win/e
