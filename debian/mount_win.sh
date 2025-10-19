21a4945002351b1369cf3dad78a95ec8d83505bc
Yang Kai <kaige.yang@163.com>
2025-10-19 23:14:17 +0800

    add opwenwrt

--- debian/mount_win.sh
+++ debian/mount_win.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
-sudo mount -t ntfs-3g UUID=000A28870004149C /mnt/win/e
-sudo mount -t ntfs-3g UUID=000EF3820008686A /mnt/win/f
+sudo mount -t ntfs-3g UUID=000A28870004149C $HOME/sda5
+sudo mount -t ntfs-3g UUID=000EF3820008686A $HOME/sda6
